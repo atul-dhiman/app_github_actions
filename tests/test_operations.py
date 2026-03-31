be3dcad01bf3122223ece1ed404a69c7b2247c02
Atul Dhiman <141116476+atul-dhiman@users.noreply.github.com>
2026-03-31 17:09:17 +0530

    Unit Case Test

---
+++ tests/test_operations.py
@@ -0,0 +1,8 @@
+from src.math_operations import add,sub,mul,div,expo
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+def test_sub():
+    assert sub(3,4)==-1
+    assert sub(1,1)==0
+    assert sub(5,2)==3
